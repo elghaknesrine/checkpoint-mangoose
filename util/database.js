37b3e6d8682177c45e08b40f727f3e39178701b2
elghaknesrine <elghaknesrine@gmail.com>
2024-03-08 18:35:09 +0100

    first commit

---
+++ util/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = mongoose.createConnection(
+);
+
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
+
+module.exports = connection;
